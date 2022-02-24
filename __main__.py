4ce1aab45a3a8c9c6612fdbfc1f055ab4e176a1a
pasmon <disssko at gmail.com>
2022-02-24 10:28:57 +0200

    Fix Python QA issues

--- __main__.py
+++ __main__.py
@@ -1,7 +1,7 @@
 """A Python Pulumi program"""
 
 import pulumi
-#import pulumi_oci as oci
+# import pulumi_oci as oci
 import pulumi_rke as rke
 
 config = pulumi.Config()
@@ -11,17 +11,22 @@ worker_public_ip = config.require("worker_public_ip")
 worker_internal_ip = config.require("worker_internal_ip")
 ssh_key_path = config.require("ssh_key_path")
 
-# Didn't get this working:
-#  pulumi:providers:oci (default_0_0_0):
-#    error: no resource plugin 'oci-v0.0.0' found in the workspace or on your $PATH, install the plugin using `pulumi plugin install resource oci v0.0.0`
+# Didn't get oci package working:
+# pulumi:providers:oci (default_0_0_0):
+# error: no resource plugin 'oci-v0.0.0' found in the workspace or on your $PATH,
+# install the plugin using `pulumi plugin install resource oci v0.0.0`
+
 # oci.core.Instance("oci-instance",
 #                 availability_domain="AD-1",
-#                 compartment_id="disssko",
+#                 compartment_id="NNNN",
 #                 shape="VM.Standard.E3.Flex")
 
+
 def write_kubeconfig(data):
-  with open("out/rke_kubeconfig", "w") as kubeconfig:
-    kubeconfig.write(data)
+    """Write kubeconfig from RKE to 'out' directory."""
+    with open("out/rke_kubeconfig", "w", encoding="utf8") as kubeconfig:
+        kubeconfig.write(data)
+
 
 rke_cluster = rke.Cluster("masterofclusters",
                           nodes=[
@@ -35,10 +40,10 @@ rke_cluster = rke.Cluster("masterofclusters",
                                                 roles=["worker"])
                           ],
                           services_kube_proxy_deprecated=rke.ClusterServicesKubeProxyDeprecatedArgs(
-                            extra_args={
-                              "healthz-bind-address": "0.0.0.0"
-                            }
-                          ),
+                              extra_args={
+                                "healthz-bind-address": "0.0.0.0"
+                              }
+                            ),
                           cluster_name="masterofclusters",
                           ssh_agent_auth=True,
                           ssh_key_path=ssh_key_path
