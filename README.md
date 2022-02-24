9cd0dade5e71d1b9b58404ef1d8a7977758241a3
pasmon <disssko at gmail.com>
2022-02-24 06:54:38 +0200

    Use RKE to provision k8s cluster to cloud

---
+++ README.md
@@ -0,0 +1,58 @@
+# Provision Kubernetes Cluster To Oracle Cloud Infrastructure
+
+## Requirements
+
+- Oracle Cloud Infrastructure or something else for 2 instances
+  - Docker in instances
+- Python
+- Pulumi
+
+## Install Rancher Kubernetes Engine (RKE)
+
+1. Create account to OCI for free, don't go for Stockholm region as currently there is no free instances available:
+   https://www.oracle.com/cloud/free/
+
+2. Create SSH keys in PEM format, either manually or letting OCI create them:
+   https://ocikb.com/create-keys-in-pem-format
+
+3. Modify `~/.oci/config` to correct path of your private SSH key
+
+4. Create 2 instances, and get their IP addresses
+
+5. Install and configure Docker:
+https://docs.docker.com/engine/install/
+
+6. Open firewall ports for RKE, both in OCI security list and in instances:
+https://rancher.com/docs/rke/latest/en/os/#ports
+
+7. Install Pulumi:
+https://www.pulumi.com/docs/get-started/install/
+
+8. Install Python packages with pipenv (https://pipenv.pypa.io/en/latest/install/#installing-pipenv):
+
+    `pipenv install`
+
+9. Activate Python virtual environment:
+
+    `pipenv shell`
+
+10. Set the IP addresses and path to your private SSH key with Pulumi:
+```
+pulumi config set master_public_ip <master public IP>
+pulumi config set master_internal_ip <master internal IP>
+pulumi config set worker_public_ip <master public IP>
+pulumi config set worker_internal_ip <master internal IP>
+pulumi config set ssh_key_path <master public IP>
+```
+
+11. Deploy RKE with Pulumi:
+
+    `pulumi up`
+
+### Ramblings
+
+OCI instance creation should've been automated with Pulumi but couldn't get it to work:
+https://github.com/pulumi-oci/pulumi-oci
+
+Was going to use RKE2 but no dice with ARM currently:
+https://github.com/rancher/rke2/issues/817
---
+++ __main__.py
@@ -0,0 +1,47 @@
+"""A Python Pulumi program"""
+
+import pulumi
+#import pulumi_oci as oci
+import pulumi_rke as rke
+
+config = pulumi.Config()
+master_public_ip = config.require("master_public_ip")
+master_internal_ip = config.require("master_internal_ip")
+worker_public_ip = config.require("worker_public_ip")
+worker_internal_ip = config.require("worker_internal_ip")
+ssh_key_path = config.require("ssh_key_path")
+
+# Didn't get this working:
+#  pulumi:providers:oci (default_0_0_0):
+#    error: no resource plugin 'oci-v0.0.0' found in the workspace or on your $PATH, install the plugin using `pulumi plugin install resource oci v0.0.0`
+# oci.core.Instance("oci-instance",
+#                 availability_domain="AD-1",
+#                 compartment_id="disssko",
+#                 shape="VM.Standard.E3.Flex")
+
+def write_kubeconfig(data):
+  with open("out/rke_kubeconfig", "w") as kubeconfig:
+    kubeconfig.write(data)
+
+rke_cluster = rke.Cluster("masterofclusters",
+                          nodes=[
+                            rke.ClusterNodeArgs(address=master_public_ip,
+                                                internal_address=master_internal_ip,
+                                                user="opc",
+                                                roles=["controlplane", "etcd", "worker"]),
+                            rke.ClusterNodeArgs(address=worker_public_ip,
+                                                internal_address=worker_internal_ip,
+                                                user="opc",
+                                                roles=["worker"])
+                          ],
+                          services_kube_proxy_deprecated=rke.ClusterServicesKubeProxyDeprecatedArgs(
+                            extra_args={
+                              "healthz-bind-address": "0.0.0.0"
+                            }
+                          ),
+                          cluster_name="masterofclusters",
+                          ssh_agent_auth=True,
+                          ssh_key_path=ssh_key_path
+                          )
+
+rke_cluster.kube_config_yaml.apply(lambda a: write_kubeconfig(data=a))
